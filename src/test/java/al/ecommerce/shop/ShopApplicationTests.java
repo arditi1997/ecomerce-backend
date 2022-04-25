a503debc2cbdd1e492f2537733e159294008af5e
Ardit Mete <ardit.mete@themobilelife.com>
2022-04-25 14:04:43 +0200

    remove test permanently

--- src/test/java/al/ecommerce/shop/ShopApplicationTests.java
+++ src/test/java/al/ecommerce/shop/ShopApplicationTests.java
@@ -3,11 +3,11 @@ package al.ecommerce.shop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ShopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
